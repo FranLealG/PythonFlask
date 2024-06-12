726546186907ad6cfac26e04abc06e6875c1acd4
Francisca Leal <falealgonz@gmail.com>
2024-06-12 17:37:15 -0400

    Tarea Visitias

---
+++ Visitas/server.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, redirect, session, request
+
+app = Flask(__name__)
+
+app.secret_key = "llave secreta"
+
+@app.route("/")
+def index():
+    if 'contador' not in session:
+        session['contador'] = 0
+    else:
+        session['contador'] += 1
+
+    if 'eliminar' not in session:
+        session['eliminar'] = 0
+
+    return render_template("index.html")
+
+@app.route("/vista_doble")
+def doble():
+    session['contador'] += 1
+    return redirect("/")
+
+@app.route("/visita_pers", methods=['post'])
+def visita_personalizada():
+    session['numero_visitas'] = int(request.form['num'])-1
+    session['contador'] += session['numero_visitas']
+    return redirect("/")
+
+@app.route("/destruir_sesion")
+def destruir():
+    if 'eliminar' in session:
+        session['eliminar'] += 1
+    
+    session.pop('contador', None)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
